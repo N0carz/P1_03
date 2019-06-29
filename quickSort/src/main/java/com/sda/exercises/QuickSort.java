7a3834a1446d9dade5d37e7af299410689eafeca
Eugeniusz Pasicznyk <chonest@wp.pl>
2019-06-29 14:36:49 +0200

    edit pom added gitignore added class

---
+++ quickSort/src/main/java/com/sda/exercises/QuickSort.java
@@ -0,0 +1,14 @@
+package com.sda.exercises;
+
+import java.util.List;
+
+public class QuickSort {
+    public List<Integer> sort(List<Integer> unsortedList) {
+
+        for (int i = 0; i < unsortedList.size(); i++) {
+
+        }
+
+        return unsortedList;
+    }
+}
