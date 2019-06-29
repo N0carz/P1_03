f9360618afc2d0447dc9d98bf099a2633add6499
Eugeniusz Pasicznyk <chonest@wp.pl>
2019-06-29 15:59:35 +0200

    edit class edit test

--- quickSort/src/main/java/com/sda/exercises/QuickSort.java
+++ quickSort/src/main/java/com/sda/exercises/QuickSort.java
@@ -3,14 +3,19 @@ package com.sda.exercises;
 import java.util.List;
 
 public class QuickSort {
-    public List<Integer> sort(List<Integer> unsortedList, Integer left, Integer right) {
+
+    public List<Integer> sort(List<Integer> unsortedList) {
+        quickSort(unsortedList, 0, unsortedList.size() - 1);
+        return unsortedList;
+    }
+
+    public void quickSort(List<Integer> unsortedList, Integer left, Integer right) {
 
         if (left < right) {
             Integer pivot = partition(unsortedList, left, right);
-            sort(unsortedList, left, pivot - 1);
-            sort(unsortedList, pivot + 1, right);
+            quickSort(unsortedList, left, pivot - 1);
+            quickSort(unsortedList, pivot + 1, right);
         }
-        return unsortedList;
     }
 
     private Integer partition(List<Integer> unsortedList, int left, int right) {
--- quickSort/src/test/java/com/sda/exercises/QuickSortTest.java
+++ quickSort/src/test/java/com/sda/exercises/QuickSortTest.java
@@ -10,17 +10,43 @@ import java.util.List;
 public class QuickSortTest {
 
     @Test
-    public void sortTest() {
+    public void quickSortTest() {
 //        given
         List<Integer> unsortedList = new ArrayList<>(Arrays.asList(5, 3, 1, 2, 4));
         QuickSort quickSort = new QuickSort();
 
 //        when
-        quickSort.sort(unsortedList, 0, unsortedList.size() -1);
+        quickSort.quickSort(unsortedList, 0, unsortedList.size() -1);
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
 
 //        then
         Assert.assertEquals(unsortedList, expected);
+    }
+
+    @Test
+    public void sortTest() {
+        QuickSort selectSort = new QuickSort();
+        List<Integer> unsortedList = new ArrayList<>(Arrays.asList(9, 7, 5, 8, 6, 2, 4, 3, 1));
+
+        //when
+        List<Integer> returned = selectSort.sort(unsortedList);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+        //then
+        Assert.assertEquals(returned, expected);
+    }
+
+    @Test
+    public void sortEmptyListTest() {
+        QuickSort bubbleSort = new QuickSort();
+        List<Integer> unsortedList = new ArrayList<>();
+
+        //when
+        List<Integer> returned = bubbleSort.sort(unsortedList);
+        List<Integer> expected = new ArrayList<>();
+
+        //then
+        Assert.assertEquals(returned, expected);
     }
 
 }
